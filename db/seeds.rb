4d9ee0d619f5043428c91cc1d052766741c90b7d
nilssvensson <nils27school@gmail.com>
2022-12-05 14:39:28 -0500

    added models and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Nils')
+User.create(username: 'Adam')
+User.create(username: 'Alex')
+
+
+Song.create(name: 'Abcd 1234', notes: ["hey", 345])
+Song.create(name: 'Hello world', notes: ["Abcd", 1234])
+
+Comment.create(description: 'Fantastic', user_id: 1, song_id: 2)
+Comment.create(description: 'Terrible', user_id: 2, song_id:1)
